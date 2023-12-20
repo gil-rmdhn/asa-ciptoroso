51a8dad7f0339baef3ffc14bde73985152861369
Gil Rmdhn <75925596+gil-rmdhn@users.noreply.github.com>
2023-12-20 14:30:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-  var scroll = new SmoothScroll('a[href*="#section-"]');
-  speed: 250;
-  var easeInOutQuint = new SmoothScroll('[data-easing="easeInOutQuint"]', { easing: 'easeInOutQuint' });
-});
