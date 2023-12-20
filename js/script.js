405ef84b8d1da112ec4eee149e8ab2fb37459f59
Gil Rmdhn <75925596+gil-rmdhn@users.noreply.github.com>
2023-12-20 14:31:20 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(function () {
+  var scroll = new SmoothScroll('a[href*="#section-"]');
+  speed: 250;
+  var easeInOutQuint = new SmoothScroll('[data-easing="easeInOutQuint"]', { easing: 'easeInOutQuint' });
+});
